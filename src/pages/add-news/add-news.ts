0dae4ca4d4ee6a570609c86491a800343ee5bf84
TsuiHinCHi <hctsuiaa@connect.ust.hk>
2018-04-09 23:15:23 +0800

    update add-news page

--- src/pages/add-news/add-news.ts
+++ src/pages/add-news/add-news.ts
@@ -1,25 +1,90 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Events, ModalController, ActionSheetController } from 'ionic-angular';
+import { ApiService } from '../../providers/api-service/api-service'
+import { ImageCropper } from '../../components/image-cropper/image-cropper'
 
-/**
- * Generated class for the AddNewsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-add-news',
   templateUrl: 'add-news.html',
 })
 export class AddNewsPage {
   title = "Create News";
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  imgURL = null;
+  user_info = {}
+  news_info ={ farm_id: null, datetime: null, title: null, description: null, image_url: null}
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    protected api: ApiService, 
+    private ev: Events,
+    private actionSheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController) {
+    this.user_info = navParams.get('user_info');
+    this.ev.subscribe('user_info', user_info => {
+      this.user_info = user_info
+    console.log(this.user_info);
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddNewsPage');
   }
 
+  OnSubmit(){
+    this.news_info.image_url = this.imgURL;
+    this.news_info.farm_id = this.user_info;
+    this.news_info.datetime = Date.now(); 
+    this.api.startQueue([
+      this.api.postNews(this.news_info)
+    ]).then(data=>{},err=>{
+        console.log(err)
+      })
+  }
+
+  presentActionSheet(type) {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Take Photo',
+          handler: () => {
+            let profileModal = this.modalCtrl.create(ImageCropper, { pickMethod: 0, type: type});
+            profileModal.onDidDismiss(data =>{
+              if(data){
+                if(type == 0){
+                  this.imgURL = data.imageURL;
+                } else {
+                  this.imgURL = data.imageURL;
+                }
+              }
+            })
+            profileModal.present();
+          }
+        },
+        {
+          text: 'Pick Image',
+          handler: () => {
+            let profileModal = this.modalCtrl.create(ImageCropper, { pickMethod: 1, type: type});
+            profileModal.onDidDismiss(data =>{
+              if(data){
+                if(type == 0){
+                  this.imgURL = data.imageURL;
+                } else {
+                  this.imgURL = data.imageURL;
+                }
+              }
+            })
+            profileModal.present();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+ 
+    actionSheet.present();
+  }
 }
