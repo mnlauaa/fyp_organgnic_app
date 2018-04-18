1032ad567c5cdc1729aaab0c189411103bdd087c
Sam Lau <mnlauaa@connect.ust.hk>
2018-04-18 23:05:30 +0800

    add partner

--- src/pages/partners/partners.ts
+++ src/pages/partners/partners.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ApiService } from '../../providers/api-service/api-service';
+import { TheMarketPage } from '../the-market/the-market'
+
 @Component({
   selector: 'page-partners',
   templateUrl: 'partners.html',
 })
 export class PartnersPage {
   title = 'Our Partners'
+  farm_list: any;
   constructor(
     public navCtrl: NavController,
-    public navParams: NavParams
+    public navParams: NavParams,
+    public api: ApiService
   ) {
+    this.api.startQueue([
+      this.api.getFarms()
+    ]).then(data=>{
+      this.farm_list = data[0]
+      console.log("farm", this.farm_list)
+    }),err=>{
+      console.log(err)
+    }
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PartnersPage');
+  onShop(id){
+    let filter_list = {
+      favourite: false,
+      selection: [id],
+      classSelect: [],
+      price_below: null,
+      price_above: null,
+      special: null
+    }
+    this.navCtrl.setRoot(TheMarketPage, {filter_list: filter_list});
   }
-
 }
